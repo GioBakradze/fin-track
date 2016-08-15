b0dabe7e322ebe62a8414fb9eae3b8e5ad301b61
GioBakradze <gio.bakradze.92@gmail.com>
2016-08-15 21:53:00 +0400

    adding categories works

--- src/js/actions/userActions.js
+++ src/js/actions/userActions.js
@@ -20,3 +20,28 @@ export function fetchCategories(uid) {
         payload: firebase.database().ref(`/users/${uid}/categories`).once('value')
     }
 }
+
+export function addNewCategory(uid, name) {
+    return function (dispatch) {
+        dispatch({
+            type: 'ADD_CATEGORY_PENDING',
+            payload: {}
+        });
+
+        var key = firebase.database().ref(`/users/${uid}/categories`).push().key;
+        firebase.database().ref(`/users/${uid}/categories/${key}`).set({
+            title: name
+        }).then(function () {
+            dispatch({
+                type: 'ADD_CATEGORY_FULFILLED',
+                payload: {}
+            });
+        });
+
+        // console.log(uid, name);
+        //
+        // firebase.database().ref(`/users/${uid}/categories`).push({
+        //     title: name
+        // });
+    }
+}
--- src/js/components/user/CategoriesLayout.js
+++ src/js/components/user/CategoriesLayout.js
@@ -1,19 +1,30 @@
 import React from "react"
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { fetchCategories } from './../../actions/userActions'
+import { fetchCategories, addNewCategory } from './../../actions/userActions'
 
-@connect((store) => ({ categories: store.categories, session: store.session }), (dispatch) => bindActionCreators({ fetchCategories }, dispatch))
+import AddCategory from './dumb/AddCategory'
+
+@connect((store) => ({ categories: store.categories, session: store.session }), (dispatch) => bindActionCreators({ fetchCategories, addNewCategory }, dispatch))
 export default class CategoriesLayout extends React.Component {
+
+    constructor() {
+        super();
+        this.add = this.add.bind(this);
+    }
 
     componentWillMount() {
         this.props.fetchCategories(this.props.session.user.uid);
     }
 
+    add(name) {
+        this.props.addNewCategory(this.props.session.user.uid, name);
+    }
+
     render () {
         return (
             <div>
-                there are cats LD
+                <AddCategory addCategory={this.add} />
             </div>
         )
     }
